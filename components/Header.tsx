23fc1114f491e214747b708e7f30cfd8bad06327
halmakey <halmakey@users.noreply.github.com>
2023-01-23 12:26:14 +0900

    add button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,19 @@
 import { SITE_TITLE } from "@/constants/values";
 import Link from "next/link";
+import { BorderButton, InvertBorderButton } from "./buttons";
 
 export default function Header() {
   return (
     <header className="text-white bg-gray-800 h-14">
       <div className="container mx-auto h-full px-4 flex flex-row items-center">
-        <Link href="/" className="text-4xl ">
-          {SITE_TITLE}
-        </Link>
+        <div className="w-full">
+          <Link href="/" className="text-4xl ">
+            {SITE_TITLE}
+          </Link>
+        </div>
+        <div>
+          <InvertBorderButton>Login</InvertBorderButton>
+        </div>
       </div>
     </header>
   );
--- components/buttons.tsx
+++ components/buttons.tsx
@@ -1,8 +1,20 @@
 import { ReactNode } from "react";
 
-export function BorderButton({ children }: { children: ReactNode }) {
+export interface ButtonProps {
+  children: ReactNode;
+}
+
+export function BorderButton({ children }: ButtonProps) {
   return (
-    <button className="transition border border-gray-800 px-4 py-2 hover:bg-gray-800 hover:text-white active:bg-gray-600">
+    <button className="transition border border-gray-800 px-4 py-2 hover:bg-gray-800 hover:text-white active:bg-gray-600 active:border-gray-600">
+      {children}
+    </button>
+  );
+}
+
+export function InvertBorderButton({ children }: ButtonProps) {
+  return (
+    <button className="transition border border-white px-4 py-2 hover:bg-white hover:text-gray-800 active:bg-gray-400 active:border-gray-400">
       {children}
     </button>
   );
