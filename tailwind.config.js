439a3e93abd8b8b76ba7e73b65e1db8b82691a46
halmakey <halmakey@users.noreply.github.com>
2023-01-29 18:16:16 +0900

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./hooks/**/*.{js,ts,jsx,tsx}",
+    "./constants/**/*.{js,ts}",
+    "./utils/**/*.{js,ts}"
   ],
   theme: {
     extend: {},
