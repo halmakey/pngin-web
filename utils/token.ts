f656ed6b5a9e060169472ea091291797e998ffb8
halmakey <halmakey@users.noreply.github.com>
2023-01-29 18:52:02 +0900

    remove console

--- utils/token.ts
+++ utils/token.ts
@@ -3,8 +3,6 @@ import jwt from 'jsonwebtoken'
 const JWT_PRIVATE_KEY = Buffer.from(process.env.PNGIN_JWT_PRIVATE_KEY!, 'base64')
 const JWT_PUBLIC_KEY = Buffer.from(process.env.PNGIN_JWT_PUBLIC_KEY!, 'base64')
 
-console.log(JWT_PUBLIC_KEY.toString('utf-8'))
-
 export function createToken(session: string, user: string, expiresIn: number): string {
   const result = jwt.sign({
     session,
