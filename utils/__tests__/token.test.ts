4b549ff99bad25928076e7d31a0e23a34714c8a4
halmakey <halmakey@users.noreply.github.com>
2023-01-30 02:55:04 +0900

    fix test

--- utils/__tests__/token.test.ts
+++ utils/__tests__/token.test.ts
@@ -10,11 +10,13 @@ test("generate token", () => {
   );
 });
 
-test("verify token: ok", () => {
+test("verify token: ok", async () => {
+  Date.now = jest.fn(() => 1674974716841);
+
   const token =
     "eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjoic2Vzc2lvbi1pZCIsInVzZXIiOiJ1c2VyLWlkIiwiaWF0IjoxNjc0OTc0NzE2LCJleHAiOjE2NzQ5NzQ3NzZ9.AO_4fQQn3f9tUquL7JeuOQFeFDY8URkSqcmpZVHXM-lctafrr-cpGP0t_1Ndk2n7ZjiMd8B2_pYo6h_ZwJZBaMlmAOeKGoIzgwrf3OIBK4he7rPmYHLa7-kbs1piCnFKiTwBhVjhX2XW5_rsCBJO0bzqAMibV7GQFD9s2IEvS5NQB2_5";
   const result = verifyToken(token);
-  expect(result).toEqual({
+  await expect(result).resolves.toEqual({
     exp: 1674974776,
     iat: 1674974716,
     session: "session-id",
@@ -22,16 +24,16 @@ test("verify token: ok", () => {
   });
 });
 
-test("verify token: ng: invalid alg", () => {
+test("verify token: ng: invalid alg", async () => {
   const token =
     "eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0K.eyJzZXNzaW9uIjoic2Vzc2lvbi1pZCIsInVzZXIiOiJ1c2VyLWlkIiwiaWF0IjoxNjc0OTc0NzE2LCJleHAiOjE2NzQ5NzQ3NzZ9.L3evL8W1KIjxagrW_33Ko68qqKkUxmqXPRBnCch4ZjA";
-  expect(() => verifyToken(token)).toThrowError(JsonWebTokenError);
+  await expect(verifyToken(token)).rejects.toThrowError(JsonWebTokenError);
 });
 
-test("verify token: ng: expired", () => {
+test("verify token: ng: expired", async () => {
   Date.now = jest.fn(() => 1674974777000);
 
   const token =
     "eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjoic2Vzc2lvbi1pZCIsInVzZXIiOiJ1c2VyLWlkIiwiaWF0IjoxNjc0OTc0NzE2LCJleHAiOjE2NzQ5NzQ3NzZ9.AO_4fQQn3f9tUquL7JeuOQFeFDY8URkSqcmpZVHXM-lctafrr-cpGP0t_1Ndk2n7ZjiMd8B2_pYo6h_ZwJZBaMlmAOeKGoIzgwrf3OIBK4he7rPmYHLa7-kbs1piCnFKiTwBhVjhX2XW5_rsCBJO0bzqAMibV7GQFD9s2IEvS5NQB2_5";
-  expect(() => verifyToken(token)).toThrowError(TokenExpiredError);
+  await expect(verifyToken(token)).rejects.toThrowError(TokenExpiredError);
 });
