4ddfcafd6d42507d0784f9a946cc03b32766df74
halmakey <halmakey@users.noreply.github.com>
2023-02-04 13:39:13 +0900

    fix check

--- utils/check-env.ts
+++ utils/check-env.ts
@@ -1,13 +1,13 @@
-import { ok } from "assert/strict";
+import { strict } from "assert";
 
 export function checkAllEnvs() {
-  ok(process.env.DISCORD_OAUTH_CLIENT_ID);
-  ok(process.env.DISCORD_OAUTH_REDIRECT_URL);
-  ok(process.env.DISCORD_OAUTH_CLIENT_SECRET);
-  ok(process.env.PNGIN_AWS_ACCESS_KEY_ID);
-  ok(process.env.PNGIN_AWS_SECRET_ACCESS_KEY);
-  ok(process.env.PNGIN_AWS_REGION);
-  ok(process.env.PNGIN_SESSION_TABLE_NAME);
-  ok(process.env.PNGIN_JWT_PRIVATE_KEY);
-  ok(process.env.PNGIN_JWT_PUBLIC_KEY);
+  strict.ok(process.env.DISCORD_OAUTH_CLIENT_ID);
+  strict.ok(process.env.DISCORD_OAUTH_REDIRECT_URL);
+  strict.ok(process.env.DISCORD_OAUTH_CLIENT_SECRET);
+  strict.ok(process.env.PNGIN_AWS_ACCESS_KEY_ID);
+  strict.ok(process.env.PNGIN_AWS_SECRET_ACCESS_KEY);
+  strict.ok(process.env.PNGIN_AWS_REGION);
+  strict.ok(process.env.PNGIN_SESSION_TABLE_NAME);
+  strict.ok(process.env.PNGIN_JWT_PRIVATE_KEY);
+  strict.ok(process.env.PNGIN_JWT_PUBLIC_KEY);
 }
