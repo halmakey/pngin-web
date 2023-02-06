2eaecacc1afce029e969b75fac8448094f7a3acc
halmakey <halmakey@users.noreply.github.com>
2023-02-07 08:19:39 +0900

    lang ja

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="ja">
       <Head>
         <meta name="description" content="PNG Museum" />
         <link rel="icon" href="/favicon.ico" />
