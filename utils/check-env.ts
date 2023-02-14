86437ad5458116bc3f6020df375b6fbacac80d8f
halmakey <halmakey@users.noreply.github.com>
2023-02-15 03:36:11 +0900

    remove check

--- utils/check-env.ts
+++ utils/check-env.ts
@@ -7,10 +7,6 @@ function check(value: string | undefined) {
 export function checkAllEnvs() {
   check(process.env.DISCORD_OAUTH_CLIENT_ID);
   check(process.env.DISCORD_OAUTH_CLIENT_SECRET);
-  check(process.env.PNGIN_AWS_ACCESS_KEY_ID);
-  check(process.env.PNGIN_AWS_SECRET_ACCESS_KEY);
-  check(process.env.PNGIN_AWS_REGION);
-  check(process.env.PNGIN_SESSION_TABLE_NAME);
   check(process.env.PNGIN_JWT_PRIVATE_KEY);
   check(process.env.PNGIN_JWT_PUBLIC_KEY);
 }
