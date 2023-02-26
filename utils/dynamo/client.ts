6d5359800392cb3a24e8ab4ac1cdf7f309db44d1
halmakey <halmakey@users.noreply.github.com>
2023-02-27 03:38:54 +0900

    fix use of credentials

--- utils/dynamo/client.ts
+++ utils/dynamo/client.ts
@@ -1,6 +1,12 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
-const client = new DynamoDBClient({ region: "ap-northeast-1" });
+const client = new DynamoDBClient({
+  region: process.env.PNGIN_AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.PNGIN_AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.PNGIN_AWS_SECRET_ACCESS_KEY!,
+  },
+});
 
 export const TableName = "pngin-web";
 export const ByTypeIndexName = "byType";
