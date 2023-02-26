482c2642adfeb1e0354c3b63b9b0b26c89f35053
halmakey <halmakey@users.noreply.github.com>
2023-02-27 03:33:10 +0900

    set default region

--- utils/dynamo/client.ts
+++ utils/dynamo/client.ts
@@ -1,8 +1,8 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
-const client = new DynamoDBClient({ region: process.env.AWS_REGION });
+const client = new DynamoDBClient({ region: "ap-northeast-1" });
 
-export const TableName = "pngin-web"
-export const ByTypeIndexName = "byType"
+export const TableName = "pngin-web";
+export const ByTypeIndexName = "byType";
 
 export default client;
