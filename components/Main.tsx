892720c24c1054b29656261fa947783d9c42eedd
halmakey <halmakey@users.noreply.github.com>
2023-03-05 17:22:11 +0900

    fix screen size

--- components/Main.tsx
+++ components/Main.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function Main({ children }: { children: ReactNode }) {
   return (
     <main className="text-gray-800 bg-white p-4 flex-1">
-      <div className="container mx-auto text-gray-800 p-4">{children}</div>
+      <div className="container mx-auto text-gray-800">{children}</div>
     </main>
   );
 }
